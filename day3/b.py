f024478cefd608283fee5112585c705d30e10ede
Daniel Mouritzen <dmrtzn@gmail.com>
2023-12-09 22:30:02 +0100

    Day 3

---
+++ day3/b.py
@@ -0,0 +1,45 @@
+import re
+
+
+def read_input():
+    with open("input.txt") as f:
+        content = f.read()
+
+    lines = []
+    for line in content.strip().splitlines():
+        if not lines:
+            lines.append("." * (len(line) + 2))
+        lines.append("." + line + ".")
+    lines.append("." * (len(line) + 2))
+    return lines
+
+
+def main():
+    lines = read_input()
+
+    gears: dict[tuple[int, int], list[int]] = {}
+    for row in range(1, len(lines) - 1):
+        for match in re.finditer(r"\d+", lines[row]):
+            # print(match.group())
+            for x in range(match.start() - 1, match.end() + 1):
+                for y in range(row - 1, row + 2):
+                    char = lines[y][x]
+                    # print(f"{x}, {y}: {char}")
+                    if char == "*":
+                        gears.setdefault((x, y), []).append(int(match.group()))
+                        # print("yes")
+                        break
+                else:
+                    continue
+                break
+
+    sum_ = 0
+    for gear in gears.values():
+        if len(gear) == 2:
+            sum_ += gear[0] * gear[1]
+
+    print(sum_)
+
+
+if __name__ == "__main__":
+    main()
